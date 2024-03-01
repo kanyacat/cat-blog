dc9cd43be35d6321a3f0b4ee1e2f699c8ccb3f08
kan <kanyakancat@gmail.com>
2024-03-01 23:26:57 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom'
+import styles from './Meow.module.scss'
+
+render(
+	<div className={styles.meow}>hello~ ффффффффффф</div>,
+	document.getElementById('root')
+)
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(arg: number): string {
+	console.log('нечто супер')
+
+	return 'meow'
+}
