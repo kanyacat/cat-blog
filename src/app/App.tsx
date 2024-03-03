3a837a06e9e12d4424c2acf8a111e4078a7d9378
kan <kanyakancat@gmail.com>
2024-03-03 12:05:52 +0300

    structure & navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,18 @@ import { useTheme } from './providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from 'app/providers/router'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 
 const App = () => {
-	const { theme, toggleTheme } = useTheme()
+	const { theme } = useTheme()
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
-			<button onClick={toggleTheme}>сменить тему</button>
-
-			<AppRouter />
+			<div className='content-page'>
+				<Sidebar />
+				<AppRouter />
+			</div>
 		</div>
 	)
 }
