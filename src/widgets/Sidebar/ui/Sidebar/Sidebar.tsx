a306d8dff7a6a0b6aa7b5d6cc690d54bb01be14f
kan <kanyakancat@gmail.com>
2024-03-08 09:23:11 +0300

    storybook

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,7 +4,6 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/ui/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-// @ts-ignore
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
