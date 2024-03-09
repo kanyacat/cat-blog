47071c78b44a071a65d8c2c73cfa1283cde2c5a6
kan <kanyakancat@gmail.com>
2024-03-09 18:26:09 +0300

    fix eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { ErrorBoundary } from 'app/providers/ErrorBoudary';
+
 import App from './app/App';
 
 import './shared/config/i18n/i18n';
-import { ErrorBoundary } from 'app/providers/ErrorBoudary';
 
 render(
     <BrowserRouter>
--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,14 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 
 describe('Button', () => {
     test('Test render', () => {
-        render(<Button>TEST</Button>);
+        const { t } = useTranslation('');
+
+        render(<Button>{t('TEST')}</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
     test('Test clear theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        const { t } = useTranslation('');
+
+        render(<Button theme={ThemeButton.CLEAR}>{t('TEST')}</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
     });
