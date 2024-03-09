62f663f426aaeb7cd345d91c4a65b398c784305c
kan <kanyakancat@gmail.com>
2024-03-09 18:44:31 +0300

    fix btn test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -4,14 +4,14 @@ import { useTranslation } from 'react-i18next';
 
 describe('Button', () => {
     test('Test render', () => {
-        const { t } = useTranslation('');
+        const { t } = useTranslation();
 
         render(<Button>{t('TEST')}</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
     test('Test clear theme', () => {
-        const { t } = useTranslation('');
+        const { t } = useTranslation();
 
         render(<Button theme={ThemeButton.CLEAR}>{t('TEST')}</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
