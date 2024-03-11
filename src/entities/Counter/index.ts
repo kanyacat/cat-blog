d21d9c934d35a7e5c7d52ae833ea72951859cfff
kan <kanyakancat@gmail.com>
2024-03-11 18:15:47 +0300

    add input, login form and modal, huski

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,6 +1,6 @@
 import { counterActions, counterReducer } from './model/slice/counterSlice';
+import { CounterSchema } from './model/types/CounterSchema';
 import { Counter } from './ui/Counter';
-import type { CounterSchema } from './model/types/counterSchema';
 
 export {
     counterReducer,
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -11,14 +11,17 @@ interface ModalProps {
 	children: ReactNode
 	isOpen?: boolean
 	onClose?: () => void
+    lazy?: boolean
 }
 
 const ANIMATION_DELAY = 300;
 
 export const Modal = ({
-    className, children, isOpen, onClose,
+    className, children, isOpen, onClose, lazy,
 }: ModalProps) => {
     const [isClosing, setIsClosing] = useState<boolean>(false);
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+
     const timeRef = useRef<ReturnType< typeof setTimeout>>();
 
     const { theme } = useTheme();
@@ -42,6 +45,14 @@ export const Modal = ({
         if (e.key === 'Escape') closeHandler();
     }, [closeHandler]);
 
+    const contentHandler = (e:React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    useEffect(() => {
+        if (isOpen) setIsMounted(true);
+    }, [isOpen]);
+
     useEffect(() => {
         if (isOpen) {
             window.addEventListener('keydown', onKeyDown);
@@ -53,9 +64,7 @@ export const Modal = ({
         };
     }, [isOpen, onKeyDown]);
 
-    const contentHandler = (e:React.MouseEvent) => {
-        e.stopPropagation();
-    };
+    if (lazy && !isMounted) return null;
 
     return (
         <Portal>
