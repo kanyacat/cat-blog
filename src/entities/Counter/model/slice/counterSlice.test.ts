f0c946701b7fb2a8f92ccf7516f0792a7ed48b3e
kan <kanyakancat@gmail.com>
2024-03-11 16:09:13 +0300

    redux toolkit

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,23 @@
+import { counterReducer, CounterSchema } from 'entities/Counter';
+import { counterActions } from 'entities/Counter/model/slice/counterSlice';
+
+describe('counterSlice.test', () => {
+    test('increment', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(counterReducer(state, counterActions.increment()))
+            .toEqual({ value: 11 });
+    });
+
+    test('decrement', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(counterReducer(state, counterActions.decrement()))
+            .toEqual({ value: 9 });
+    });
+
+    test('should work with empty state', () => {
+        expect(counterReducer(undefined, counterActions.decrement()))
+            .toEqual({ value: -1 });
+    });
+});
