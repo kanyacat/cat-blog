7ebd81a1c9a93e67aa5ec9a9eeee3aa37e7920f4
kan <kanyakancat@gmail.com>
2024-03-13 17:34:20 +0300

    add login module

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,1 +1,3 @@
 export { LoginModal } from './ui/LoginModal/LoginModal';
+export { LoginSchema } from './model/types/LoginSchema';
+export {} from './model/slice/loginSlice';
--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Navbar } from './Navbar';
 
 export default {
@@ -16,6 +17,15 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({
+    user: {},
+})];
 
 export const Dark = Template.bind({});
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+
+export const Auth = Template.bind({});
+Auth.args = {};
+Auth.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
