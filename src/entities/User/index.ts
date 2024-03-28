a24478eda7d10fe864d2868fc9f4cf17573102cd
kan <kanyakancat@gmail.com>
2024-03-28 16:07:43 +0300

    theme and update router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -5,6 +5,8 @@ export {
     userActions,
 } from './model/slice/userSlice';
 
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
+
 export {
     UserSchema,
     User,
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -5,7 +5,7 @@ import { NotFoundPage } from 'pages/NotFoundPage';
 
 import { ProfilePage } from 'pages/ProfilePage';
 
-type AppRoutesProps = RouteProps & {
+export type AppRoutesProps = RouteProps & {
     authOnly?: boolean,
 };
 
